061ef6dc80f891aa133b79c21b607a6e2e1f2f02
Roberto Rodriguez <roberto.rodriguez@alumni.colostate.edu>
2020-08-19 17:04:08 -0600

    Coverage commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_18_212208) do
+ActiveRecord::Schema.define(version: 2020_08_19_220650) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
