dd6f4ed9d0ce3983f22a4410ee06fe88776f8312
Roberto Rodriguez <roberto.rodriguez@alumni.colostate.edu>
2020-08-19 19:55:04 -0600

    Complete user story 5, ability to update a shelter

--- spec/features/edit_shelter_spec.rb
+++ spec/features/edit_shelter_spec.rb
@@ -8,6 +8,13 @@ RSpec.describe 'Edit Shelter' do
 
     click_link "Update Shelter"
 
-    expect(current_path).to eq('/shelters/#{shelter1.id}/edit')
+    expect(current_path).to eq("/shelters/#{shelter1.id}/edit")
+
+    fill_in "shelter[name]", with:"Potato"
+    fill_in "shelter[address]", with:"123 Main Street"
+    fill_in "shelter[city]", with:"Denver"
+    fill_in "shelter[state]", with:"CO"
+    fill_in "shelter[zip]", with:"12345"
+    click_on "Update Shelter"
   end
 end
